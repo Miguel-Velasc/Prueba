b2062e4496579b132f3c7a28a6668439a901b579
Miguel-Velasc <a_miguel.velasco.fernandez@iespablopicasso.es>
2023-11-28 21:42:54 +0100

    Primer commit

---
+++ NumAleatorios.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class NumAleatorios {
+
+    public static void main(String[] args) {
+        mostrarNumerosAleatorios();
+    }
+
+    public static void mostrarNumerosAleatorios() {
+        Random random = new Random();
+
+        System.out.println("20 números aleatorios entre 1 y 10:");
+
+        for (int i = 0; i < 20; i++) {
+            int numeroAleatorio = random.nextInt(10) + 1;
+            System.out.print(numeroAleatorio + " ");
+        }
+    }
+}
